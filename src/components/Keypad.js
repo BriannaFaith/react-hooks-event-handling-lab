8d8bb9789cd0bb475a6c5a6d3ec7c92dc0960112
FaithOgendi <faithbrianna01@gmail.com>
2024-02-03 05:38:51 +0300

    unfinished

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,11 @@
 // Code Keypad Component Here
-
+import React from 'react'
 function Keypad (){
     return (
-        <div></div>
+        <div>
+        <input type="password" onChange={handleChange}></input>
+        <button>Enter</button>
+    </div>
     )
 }
 
